93eb119aa845d9afbb411e33ebe030819a29f732
RenatoDias22 <lrds22@hotmail.com>
2017-03-19 18:01:13 -0300

    Subindo tela inicial, tela de login e tela de cadastro.

---
+++ app/src/main/java/com/example/renato/spotglamapp/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.renato.spotglamapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void ClickLoginFacebook(View view) {
+    }
+
+    public void clickLoginEntrar(View view) {
+    }
+}
--- app/src/main/java/com/example/renato/spotglamapp/MainActivity.java
+++ app/src/main/java/com/example/renato/spotglamapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.renato.spotglamapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickCadastrar(View v){
+        Intent intent = new Intent(this, CadastrarActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickDoBotaoEntrar(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
