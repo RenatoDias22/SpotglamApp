ebef6227c5dfef6369c9cc7b79b1441ff1fb94d4
Renato Dias <lrds22@hotmail.com>
2017-05-30 15:54:49 -0300

    Subindo Ajuda, Sobre e parte da tela de Promocao

--- app/src/main/java/com/example/renato/spotglamapp/MenuLateralActivity.java
+++ app/src/main/java/com/example/renato/spotglamapp/MenuLateralActivity.java
@@ -4,13 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -26,7 +24,6 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuLateralActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
     FirebaseAuth auth;
 
     @Override
--- app/src/main/java/com/example/renato/spotglamapp/activitys/menu/Promocao/PromocaoFragment.java
+++ app/src/main/java/com/example/renato/spotglamapp/activitys/menu/Promocao/PromocaoFragment.java
@@ -7,15 +7,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.renato.spotglamapp.R;
 
-public class PromocaoFragment extends Fragment {
+import java.util.List;
+
+public class PromocaoFragment extends Fragment implements AdapterView.OnItemClickListener {
+
+    ListView lista;
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        lista= (ListView) getActivity().findViewById(R.id.lista);
+//        return inflater.inflate(R.layout.fragment_promocao, container, false);
+//    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_promocao, container, false);
+        lista= (ListView) getActivity().findViewById(R.id.lista);
+        return view;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_promocao, container, false);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Planets, android.R.layout.simple_list_item_1);
+//        lista.setAdapter(adapter);
+//        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
     }
 }
